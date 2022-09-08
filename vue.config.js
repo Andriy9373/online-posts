8e33d602ca6d532ca8f87d8e38997b6a07e417fe
Hirniak Andrii <hirniak.andrii@gmail.com>
2022-09-08 22:20:28 +0300

    Fixed output dir

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-const path = require('path');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, '/public'),
   devServer: {
     proxy: {
       '/api': {
